b131f9ec30418bdf36c617e81c912afe11039f56
Denis Ortega <ortega.dc98@gmail.com>
2020-06-29 00:44:36 -0400

    testing...

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function getMainChat(groupId) {
 
     axios.get(`https://api.line.me/v2/bot/group/${groupId}/members/count`).then((res) => {
         axios.get(`https://api.line.me/v2/bot/group/${groupId}/summary`).then(res2 => {
-            if(res.data.count > 10 && res2.groupName == "The Groupies" && mainChatId == null) {
+            if(res.data.count > 10 && res2.data.groupName == "The Groupies" && mainChatId == null) {
                 mainChatId = groupId;
                 happyBirthday(); //start happy birthday clock
             }
@@ -26,6 +26,7 @@ function getMainChat(groupId) {
             }
             console.log(res2, mainChatId, otherChatId);
         })
+        .catch(err => console.log(err))
     })
     .catch((err) => console.log(err));
 }
@@ -100,7 +101,10 @@ function _pushTextMessage(id, _messages) {
     axios.post(`https://api.line.me/v2/bot/message/push`, {
         to: id,
         messages: messages
+    }).then(res => {
+        console.log(`Sent bday message to ${id}`);
     })
+    .catch(err => console.log(err))
 }
 
 function getUser(groupId, userId) {
